3bafe8a5586f54da03eeb8aa7c4113e90d1fc32a
USMAN DEMBÉLÉ <145369069+barancibia23@users.noreply.github.com>
2024-01-11 20:16:16 +0100

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+
+
+       /*  for (int N = -20; N <= 20; N++) {
+            if (N % (0+N) == 1 || N % (1) == 1) {
+                System.out.println(N);    
+            }
+        } System.out.println("lo rial g for laif"); */
+        
+        for (int n = -20; n <= 20; n++) {
+            if (n % 2 == 1) {
+                
+            }
+        }
+    }
+}
